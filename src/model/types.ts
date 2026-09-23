b2aefec59b09a05680edef92390f2b6adb87af68
xachris <99147041+xachris@users.noreply.github.com>
2026-09-23 20:20:30 +0800

    feat: initialize CreatiBox MVP foundation

---
+++ src/model/types.ts
@@ -0,0 +1,45 @@
+export type EntityKind = 'car' | 'road' | 'wall' | 'obstacle' | 'start' | 'finish'
+export type EntityState = 'Idle' | 'Moving' | 'Damaged' | 'Broken' | 'Finished'
+
+export interface Vec2 {
+  x: number
+  y: number
+}
+
+export interface Entity {
+  id: string
+  kind: EntityKind
+  name: string
+  position: Vec2
+  size: Vec2
+  rotation: number
+  color: number
+  movable: boolean
+  state: EntityState
+  durability: number
+  maxDurability: number
+  speed: number
+  maxSpeed: number
+}
+
+export interface Rule {
+  id: string
+  sourceKind: EntityKind
+  interaction: 'collide' | 'reach'
+  targetKind: EntityKind
+  effect: 'damage' | 'finish'
+  value?: number
+}
+
+export interface World {
+  id: string
+  name: string
+  entities: Entity[]
+  rules: Rule[]
+}
+
+export interface CreatiBoxProject {
+  formatVersion: '0.1'
+  name: string
+  world: World
+}
